9496fdfb9ad934637f5e6459e7f3c244d9a76c89
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-20 20:28:47 -0300

    User card

---
+++ lib/views/home.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todos/widgets/user-card.widget.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          UserCard(),
+        ],
+      ),
+    );
+  }
+}
