8d0db01e165b7b95baec67eda52e3d90b7a1d763
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-21 21:45:51 -0300

    todo repository

---
+++ lib/repositories/todo.repository.dart
@@ -0,0 +1,57 @@
+import 'package:todos/models/todo-item.model.dart';
+
+class TodoRepository {
+  Future<List<TodoItem>> getTodayTodos() async {
+    var todos = new List<TodoItem>();
+    todos.add(
+      new TodoItem(
+        id: "123456",
+        title: "Hoje",
+        done: false,
+        date: DateTime.now(),
+      ),
+    );
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    return todos;
+  }
+
+  Future<List<TodoItem>> getTomorrowTodos() async {
+    var todos = new List<TodoItem>();
+    todos.add(
+      new TodoItem(
+        id: "123456",
+        title: "Amanhã",
+        done: false,
+        date: DateTime.now(),
+      ),
+    );
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    return todos;
+  }
+
+  Future<List<TodoItem>> getAllTodos() async {
+    var todos = new List<TodoItem>();
+    todos.add(
+      new TodoItem(
+        id: "123456",
+        title: "Hoje",
+        done: false,
+        date: DateTime.now(),
+      ),
+    );
+
+    todos.add(
+      new TodoItem(
+        id: "1234567",
+        title: "Amanhã",
+        done: false,
+        date: DateTime.now(),
+      ),
+    );
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    return todos;
+  }
+
+  Future<List<TodoItem>> add(TodoItem item) async {}
+  Future<List<TodoItem>> markAsDone(TodoItem item) async {}
+}
