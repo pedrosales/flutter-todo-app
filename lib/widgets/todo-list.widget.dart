178026dcf7803ff2e64acba3f626d80d3da71867
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-21 22:49:44 -0300

    listagemdos itens

--- lib/widgets/todo-list.widget.dart
+++ lib/widgets/todo-list.widget.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:todos/controllers/todo.controller.dart';
+import 'package:todos/stores/app.store.dart';
+import 'package:todos/widgets/busy.widget.dart';
 
 class TodoList extends StatelessWidget {
+  final _dateFormat = new DateFormat('dd/MM/yyyy');
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.only(
-        left: 40,
+    final store = Provider.of<AppStore>(context);
+    final controller = new TodoController(store);
+
+    return Observer(
+      builder: (_) => TDBusy(
+        busy: store.busy,
+        child: store.todos.length == 0
+            ? Center(
+                child: Text(
+                  "Nenhuma tarefa encontrada",
+                ),
+              )
+            : ListView.builder(
+                padding: EdgeInsets.only(
+                  left: 40,
+                ),
+                itemCount: store.todos.length,
+                itemBuilder: (context, index) {
+                  var todo = store.todos[index];
+                  return ListTile(
+                    title: Text(
+                      todo.title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: todo.done
+                            ? Colors.black.withOpacity(0.2)
+                            : Colors.black,
+                      ),
+                    ),
+                    subtitle: Text(
+                      _dateFormat.format(todo.date),
+                    ),
+                  );
+                },
+              ),
       ),
-      children: <Widget>[
-        ListTile(
-          title: Text(
-            "Ir ao supermercado",
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black,
-            ),
-          ),
-          subtitle: Text(
-            "02/02/2020",
-          ),
-        ),
-        ListTile(
-          title: Text(
-            "Ir a Academia",
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black.withOpacity(0.2),
-            ),
-          ),
-          subtitle: Text(
-            "02/02/2020",
-          ),
-        ),
-      ],
     );
   }
 }
