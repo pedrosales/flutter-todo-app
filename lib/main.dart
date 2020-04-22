c794b961d12722891a464e8abda28e1c1fb64ccb
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-21 21:34:03 -0300

    aceitando chamadas https

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todos/stores/app.store.dart';
 import 'package:todos/themes/app.theme.dart';
 import 'package:todos/views/login.view.dart';
 
-void main() => runApp(MyApp());
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    HttpClient client = super.createHttpClient(context);
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => true;
+    return client;
+  }
+}
+
+void main() {
+  HttpOverrides.global = new MyHttpOverrides();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
